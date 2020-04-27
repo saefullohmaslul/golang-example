9a6fc079846374d15cd3e0d020274d071789e608
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-04-27 13:26:21 +0700

    add routes and response mock

---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/saefullohmaslul/Golang-Example/controllers"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Router middleware
+func Router(g *echo.Group) {
+	g.GET("/name", controllers.GetName)
+}
---
+++ utils/response.go
@@ -0,0 +1,7 @@
+package utils
+
+// Response mock
+type Response struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+}
