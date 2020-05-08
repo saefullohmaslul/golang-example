f159c60bdaf9cc1753294b5153ba4895c6c07d20
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-05-08 10:28:53 +0700

    add route not found

--- src/app/application.go
+++ src/app/application.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	db "github.com/saefullohmaslul/golang-example/src/database"
 	"github.com/saefullohmaslul/golang-example/src/database/migration"
@@ -29,6 +31,16 @@ func (a Application) CreateTest(r *gin.Engine) {
 func configureAPIEndpoint(r *gin.Engine) {
 	g := r.Group("/")
 	routes.Router(g)
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status": http.StatusNotFound,
+			"flag":   "NOT_FOUND",
+			"errors": gin.H{
+				"message": "The route you are looking for is not found",
+				"flag":    "ROUTE_NOT_FOUND",
+			},
+		})
+	})
 }
 
 func configureAppDB() {
