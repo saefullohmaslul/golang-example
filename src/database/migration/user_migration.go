e84b21aeff499007fdf2d40a43a8c6efef109b04
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2020-05-14 12:24:31 +0700

    change migration to automigrate

--- src/database/migration/user_migration.go
+++ src/database/migration/user_migration.go
@@ -3,9 +3,12 @@ package migration
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/saefullohmaslul/golang-example/src/database/entity"
+	"github.com/sirupsen/logrus"
 )
 
 // CreateUser is create user tabel for migration
 func CreateUser(conn *gorm.DB) {
-	conn.CreateTable(&entity.User{})
+	conn.AutoMigrate(&entity.User{})
+
+	logrus.Info("Success running migration")
 }
